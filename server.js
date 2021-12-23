e4c6ee489325ff1d3e2db1c787be90e5e7bec1d9
Alex Rusu <alexandru.rusu@student.fairfield.edu>
2021-12-23 11:35:09 -0500

    another try to fix file

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 app.use(cors())
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/FinalExam'));
-app.get('*', function(req,res) {
-res.sendFile(path.join(__dirname + "/dist/FinalExam/src/index.html"));   
-//res.sendFile(path.join(__dirname + '/dist/FinalExam/src/index.html'));
+app.get('/*', function(req,res) {
+//res.sendFile(path.join(__dirname + "/dist/FinalExam/src/index.html"));   
+res.sendFile(path.join(__dirname + '/dist/FinalExam/src/index.html'));
 //res.sendFile(path.join(__dirname + "/dist/FinalExam/index.html"));     
-//res.sendFile(path.join(__dirname + '/dist/FinalExam/index.html'));
+//res.sendFile(path.join(__dirname + '/dist/FinalExam/index.html')); original
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
